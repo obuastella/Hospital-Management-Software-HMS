1630720edafdece7b1d076493d061d3813cf60de
Obua Stella <stellaobua123@gmail.com>
2024-06-06 22:58:40 +0000

    fixed issues

--- hms/app/page.tsx
+++ hms/app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function page() {
   return (
     <main>
       <h1 className="text-green-300 font-bold text-center my-10 text-4xl">Landing page here</h1>
