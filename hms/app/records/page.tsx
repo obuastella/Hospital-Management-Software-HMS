1421efe045f1a82c6433b0e14b0233bd5505068f
Obua Stella <stellaobua123@gmail.com>
2024-06-08 18:49:44 +0000

    installing shadcn and updating color variables

---
+++ hms/app/records/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+    return(
+        <main>
+            <h1 className="text-primary text-center my-20 text-4xl font-bold">Electronic Helath Care record  HERE</h1>
+        </main>
+    )
+}
