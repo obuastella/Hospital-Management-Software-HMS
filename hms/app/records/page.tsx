85aefd17ad0db10e8a25ac9978f3802e8b4b96ef
Obua Stella <stellaobua123@gmail.com>
2024-06-09 16:41:51 +0000

    dashboard setup

--- hms/app/records/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function page() {
-    return(
-        <main>
-            <h1 className="text-primary text-center my-20 text-4xl font-bold">Electronic Helath Care record  HERE</h1>
-        </main>
-    )
-}
