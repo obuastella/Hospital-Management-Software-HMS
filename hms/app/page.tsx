d689b7a9c60455ae3ec91e321d4580cbf065a35a
Omojola Zion <omojolazion@gmail.com>
2024-06-09 17:25:14 +0100

    Created website Navbar and added a dummy logo

--- hms/app/page.tsx
+++ hms/app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./LandingPage/Navbar/navbar";
+
 export default function page() {
   return (
     <main>
-      <h1 className="text-green-300 font-bold text-center my-10 text-4xl">Landing page here</h1>
+      < Navbar />
     </main>
   );
 }
