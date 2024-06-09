05aa2fae5d9af2e11726777e6ebf121628a03863
Omojola Zion <omojolazion@gmail.com>
2024-06-09 20:58:23 +0100

    Comment

--- hms/app/page.tsx
+++ hms/app/page.tsx
@@ -4,7 +4,6 @@ import HeroSection from "./LandingPage/Sections/heroSection";
 export default function page() {
   return (
     <main>
-      {/*  */}
       < Navbar />
       < HeroSection />
     </main>
