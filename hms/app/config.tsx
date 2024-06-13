574170f99e5e6e3c51caef4d0abffaae26b96f46
Obua Stella <stellaobua123@gmail.com>
2024-06-13 05:08:53 +0100

    Organisaztion view wip

--- hms/app/config.tsx
+++ hms/app/config.tsx
@@ -45,3 +45,55 @@ export const NavItems = () => {
     },
   ];
 };
+
+export const OrgNavItems = () => {
+  const pathname = usePathname();
+
+  function isNavItemActive(pathname: string, nav: string) {
+    return pathname.includes(nav);
+  }
+  return [
+    {
+      name: "Dashboard",
+      href: "/dashboard",
+      icon: <Home size={20} />,
+      active: pathname === "/dashboard",
+      position: "top",
+    },
+    {
+      name: "Patients",
+      href: "/patients",
+      icon: <User size={20} />,
+      active: isNavItemActive(pathname, "/userprofile"),
+      position: "top",
+    },
+    {
+      name: "Doctors",
+      href: "/doctors",
+      icon: <User size={20} />,
+      active: isNavItemActive(pathname, "/doctors"),
+      position: "top",
+    },
+    {
+      name: "Appointments",
+      href: "/docappointments",
+      icon: <Calendar size={20} />,
+      active: isNavItemActive(pathname, "/docappointments"),
+      position: "top",
+    },
+    {
+      name: "Support",
+      href: "/contactus",
+      icon: <Phone size={20} />,
+      active: isNavItemActive(pathname, "/contactus"),
+      position: "top",
+    },
+    {
+      name: "Logout",
+      href: "/",
+      icon: <LogOut size={20} />,
+      active: isNavItemActive(pathname, "/logout"),
+      position: "bottom",
+    },
+  ];
+};
